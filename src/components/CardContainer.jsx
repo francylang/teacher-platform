171a2a22e03cbcc63e6afc0917ac9d2ccb2a5736
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-12-26 12:55:59 -0700

    Fetch in app component

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import DiscussionCard from './DiscussionCard';
-import { fetchDiscussions } from '../utils/getDiscussions';
-/*eslint-disable*/
+
 class CardContainer extends Component {
   constructor() {
     super();
@@ -10,36 +9,8 @@ class CardContainer extends Component {
     };
   };
 
-  componentDidMount() {
-    this.getDiscussions();
-  };
-
-
-  async getDiscussions() {
-    // try {
-    //   const discussions =  fetchDiscussions();
-    //   console.log('in component: ', discussions);
-    //   this.setState({ discussions })
-    // } catch (error) {
-    //   console.log({ error });
-    // }
-    // await fetchDiscussions()
-    //   .then((discussions) => {
-    //     discussions.forEach(discussion => {
-    //       console.log(discussion);
-    //       return (`
-    //         <article className="card">
-    //           <h4>${discussion.title}</h4>
-    //           <p>${discussion.body}</p>
-    //           <p>${discussion.topicTag}</p>
-    //         </article>
-    //       `);
-    //     })
-    //   })
-    //   .catch((error) => console.error({ error }));
-  };
-
   render() {
+    // console.log(this.props);
     return (
       <div>
         <h2 className="current-feed-title">Discussions:</h2>
