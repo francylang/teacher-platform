8dd1570534fb6535fdff64ae2bdd6121bc123845
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-12-29 13:15:03 -0700

    Toggle between 6 7 or 8 grade standards on nav

--- src/components/FilterForm.jsx
+++ src/components/FilterForm.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { math6RP, math6NS, math6EE, math6G, math6SP, math7RP, math7NS, math7EE, math7G, math7SP, math8NS, math8EE, math8F, math8G, math8SP } from '../utils/standards';
+import { math6RP, math6NS, math6EE, math6G, math6SP, math7RP, math7NS, math7EE, math7G, math7SP, math8NS, math8EE, math8F, math8G, math8SP, mathSix, mathSeven, mathEight } from '../utils/standards';
 
 class FilterForm extends Component {
   constructor() {
     super();
     this.state = {
       selected: [],
-      showingMath6: false,
+      showingMath6: true,
       showingMath7: false,
       showingMath8: false,
     };
@@ -15,61 +15,63 @@ class FilterForm extends Component {
   showOptions(standardsArray, state) {
     if (state) {
       return standardsArray.map(standard => {
-        return (
-          <div className="standard-checkbox">
-            <input type="checkbox" id={standard} name={standard} value={standard}></input>
-            <label htmlFor={standard}>{standard}</label>
-          </div>
-        );
-      })
+        let firstChar = standard.charAt(0);
+        if (firstChar === '6' || firstChar === '7' || firstChar === '8') {
+          return (
+            <div className="standard-checkbox" id={firstChar} >
+              <input type="checkbox" id={standard} name={standard} value={standard}></input>
+              <label htmlFor={standard}>{standard}</label>
+            </div>
+          );
+        } else {
+          return (
+            <h4>{standard}</h4>
+          );
+        }
+      });
     }
   };
 
-  showMath6Tags(gradeLevel) {
-    this.setState({ showingMath6: !this.state.showingMath6 });
+  showMath6Tags() {
+    this.setState({ showingMath6: true, showingMath7: false, showingMath8: false });
   };
 
   showMath7Tags() {
-    this.setState({ showingMath7: !this.state.showingMath7 });
+    console.log('show math 7');
+    this.setState({ showingMath6: false, showingMath7: true, showingMath8: false });
   };
 
   showMath8Tags() {
-    this.setState({ showingMath8: !this.state.showingMath8 });
+    console.log('show math 8');
+    this.setState({ showingMath6: false, showingMath7: false, showingMath8: true });
   };
 
   render() {
     return (
       <section>
-        <h3>Select one or more topics to filter by:</h3>
-        <h4>Math 6</h4>
+        <h3>Click on a grade level tab to choose standard(s):</h3>
         <button onClick={() => this.showMath6Tags()}>
-          Show all tags for grade 6
+          Grade 6
         </button>
-        { this.showOptions(math6RP, this.state.showingMath6) }
-        { this.showOptions(math6NS, this.state.showingMath6) }
-        { this.showOptions(math6EE, this.state.showingMath6) }
-        { this.showOptions(math6G, this.state.showingMath6) }
-        { this.showOptions(math6SP, this.state.showingMath6) }
-
-        <h4>Math 7</h4>
         <button onClick={() => this.showMath7Tags()}>
-          Show all tags for grade 7
+          Grade 7
         </button>
-        { this.showOptions(math7RP, this.state.showingMath7) }
-        { this.showOptions(math7NS, this.state.showingMath7) }
-        { this.showOptions(math7EE, this.state.showingMath7) }
-        { this.showOptions(math7G, this.state.showingMath7) }
-        { this.showOptions(math7SP, this.state.showingMath7) }
-
-        <h4>Math 8</h4>
         <button onClick={() => this.showMath8Tags()}>
-          Show all tags for grade 8
+          Grade 8
         </button>
-        { this.showOptions(math8F, this.state.showingMath8) }
-        { this.showOptions(math8NS, this.state.showingMath8) }
-        { this.showOptions(math8EE, this.state.showingMath8) }
-        { this.showOptions(math8G, this.state.showingMath8) }
-        { this.showOptions(math8SP, this.state.showingMath8) }
+        <section className="options-container">
+          <article className="options">
+            { this.showOptions(mathSix, this.state.showingMath6) }
+          </article>
+          <article className="options">
+            { this.showOptions(mathSeven, this.state.showingMath7) }
+          </article>
+          <article className="options">
+            { this.showOptions(mathEight, this.state.showingMath8) }
+          </article>
+        </section>
+
+        <button>FIND discussions based on what I checked!!</button>
       </section>
     );
   }
--- src/utils/standards.js
+++ src/utils/standards.js
@@ -1,5 +1,45 @@
+export const mathSix = [
+  "Ratios & Proportions",
+  "6.RP.A.1",
+  "6.RP.A.2",
+  "6.RP.A.3",
+  "6.RP.A.3.A",
+  "6.RP.A.3.B",
+  "6.RP.A.3.C",
+  "The Number System",
+  "6.NS.A.1",
+  "6.NS.B.2",
+  "6.NS.B.3",
+  "6.NS.B.4",
+  "6.NS.C.5",
+  "6.NS.C.6",
+  "6.NS.C.7",
+  "6.NS.C.8",
+  "Expressions & Equations",
+  "6.EE.A.1",
+  "6.EE.A.2",
+  "6.EE.A.3",
+  "6.EE.A.4",
+  "6.EE.B.5",
+  "6.EE.B.6",
+  "6.EE.B.7",
+  "6.EE.B.8",
+  "6.EE.C.9",
+  "Geometry & Measurement",
+  "6.G.A.1",
+  "6.G.A.2",
+  "6.G.A.3",
+  "6.G.A.4",
+  "Statistics & Probability",
+  "6.SP.A.1",
+  "6.SP.A.2",
+  "6.SP.A.3",
+  "6.SP.A.4",
+  "6.SP.B.5",
+];
+
 export const math6RP = [
-  "Ratios and Proportions",
+  "Ratios & Proportions",
   "6.RP.A.1",
   "6.RP.A.2",
   "6.RP.A.3",
@@ -21,7 +61,7 @@ export const math6NS = [
 ];
 
 export const math6EE = [
-  "Expressions and Equations",
+  "Expressions & Equations",
   "6.EE.A.1",
   "6.EE.A.2",
   "6.EE.A.3",
@@ -34,7 +74,7 @@ export const math6EE = [
 ];
 
 export const math6G = [
-  "Geometry and Measurement",
+  "Geometry & Measurement",
   "6.G.A.1",
   "6.G.A.2",
   "6.G.A.3",
@@ -48,10 +88,38 @@ export const math6SP = [
   "6.SP.A.3",
   "6.SP.A.4",
   "6.SP.B.5",
-  "6.SP.B.5.A",
-  "6.SP.B.5.B",
-  "6.SP.B.5.C",
-  "6.SP.B.5.D",
+];
+
+export const mathSeven = [
+  "Ratios & Proportions",
+  "7.RP.A.1",
+  "7.RP.A.2",
+  "7.RP.A.3",
+  "The Number System",
+  "7.NS.A.1",
+  "7.NS.A.2",
+  "7.NS.A.3",
+  "Expressions & Equations",
+  "7.EE.A.1",
+  "7.EE.A.2",
+  "7.EE.A.3",
+  "7.EE.A.4",
+  "Geometry & Measurement",
+  "7.G.A.1",
+  "7.G.A.2",
+  "7.G.A.3",
+  "7.G.B.4",
+  "7.G.B.5",
+  "7.G.B.6",
+  "Statistics & Probability",
+  "7.SP.A.1",
+  "7.SP.A.2",
+  "7.SP.B.3",
+  "7.SP.B.4",
+  "7.SP.C.5",
+  "7.SP.C.6",
+  "7.SP.C.7",
+  "7.SP.C.8",
 ];
 
 export const math7RP = [
@@ -77,7 +145,7 @@ export const math7EE = [
 ];
 
 export const math7G = [
-  "Geometry and Measurement",
+  "Geometry & Measurement",
   "7.G.A.1",
   "7.G.A.2",
   "7.G.A.3",
@@ -96,6 +164,42 @@ export const math7SP = [
   "7.SP.C.6",
   "7.SP.C.7",
   "7.SP.C.8",
+];
+
+export const mathEight = [
+  "The Number System",
+  "8.NS.A.1",
+  "8.NS.A.2",
+  "Expressions & Equations",
+  "8.EE.A.1",
+  "8.EE.A.2",
+  "8.EE.A.3",
+  "8.EE.A.4",
+  "8.EE.B.5",
+  "8.EE.B.6",
+  "8.EE.C.7",
+  "8.EE.C.8",
+  "Functions",
+  "8.F.A.1",
+  "8.F.A.2",
+  "8.F.A.3",
+  "8.F.B.4",
+  "8.F.B.5",
+  "Geometry & Measurement",
+  "8.G.A.1",
+  "8.G.A.2",
+  "8.G.A.3",
+  "8.G.A.4",
+  "8.G.A.5",
+  "8.G.B.6",
+  "8.G.B.7",
+  "8.G.B.8",
+  "8.G.C.9",
+  "Statistics & Probability",
+  "8.SP.A.1",
+  "8.SP.A.2",
+  "8.SP.A.3",
+  "8.SP.B.4",
 ];
 
 export const math8NS = [
@@ -126,7 +230,7 @@ export const math8F = [
 ];
 
 export const math8G = [
-  "Geometry and Measurement",
+  "Geometry & Measurement",
   "8.G.A.1",
   "8.G.A.2",
   "8.G.A.3",
