7aef5fd0e7a9355910412161fd16a55b53883f35
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-12-30 08:23:04 -0700

    Style discussion form

--- src/components/DiscussionForm.jsx
+++ src/components/DiscussionForm.jsx
@@ -26,28 +26,33 @@ class DiscussionForm extends Component {
     return (
       <article className="discussion-form-section">
         <h4 className="new-discussion-option">New Discussion</h4>
-        <form action="" method="get">
-          <label htmlFor="GET-dicussion-title">Quick Description:
+        <form action="" method="get" className="form">
+          <label htmlFor="dicussion-title">
             <input
-              id="GET-dicussion-title"
+              id="dicussion-title"
               type="text"
               name="title"
+              placeholder="Title"
               onChange={this.handleChange.bind(this, 'title')}
-            />
+            >
+            </input>
           </label>
-          <label htmlFor="GET-dicussion-body">Detailed Question:
-            <input
-              id="GET-dicussion-body"
+          <label htmlFor="dicussion-body">
+            <textarea
+              id="dicussion-body"
               type="text"
               name="body"
-            />
+              placeholder="Detailed question"
+            >
+            </textarea>
           </label>
           <input
-            className="submit-discussion"
+            className="submit-discussion-btn"
             type="submit"
             name="submit"
             onClick={(event) => this.handleSubmit(event)}
-          />
+          >
+          </input>
         </form>
       </article>
     )
