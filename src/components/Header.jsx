49706a8d723d8203219708e80b3f6b2710036e1b
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-12-29 20:51:49 -0700

    Move sign in/out to nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ class Header extends Component {
     return (
       <header className="header">
         <h1 className="app-name">Teachzilla</h1>
-        <h3 className="sign-out">Sign Out</h3>
       </header>
     );
   }
