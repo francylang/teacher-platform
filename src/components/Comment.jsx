891c16facc9e0a113dfe854b9c252b89c13b4cdb
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-01-01 16:30:26 -0700

    Add name and date to comments and style consistently

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -8,9 +8,17 @@ class Comment extends Component {
         <p className="comment">
           I am the comment and I am a really great comment!!!
         </p>
+        <ul>
+          <li>From: Amy</li>
+          <li>Date: December 28, 2018</li>
+        </ul>
         <p className="comment">
           I am ANOTHER comment and I am a really great comment!!!
         </p>
+        <ul>
+          <li>From: Amy</li>
+          <li>Date: December 28, 2018</li>
+        </ul>
       </article>
     );
   }
