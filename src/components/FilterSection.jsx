7903857379b1e5fc553721a39d48c2933e556676
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-01-02 13:41:44 -0700

    Change all checkboxes to links

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
 
 class FilterSection extends Component {
+  something() {
+    console.log('hello');
+  }
+
   buildDomainList() {
     return this.props.domain.map(standard => {
       let firstChar = standard.charAt(0);
 
       if (firstChar === '6' || firstChar === '7' || firstChar === '8') {
         return (
-          <div className="standard-checkbox pretty p-icon p-round p-smooth">
-            <input
-              type="checkbox"
-              id={standard}
-              name={standard}
-              value={standard}
-            >
-            </input>
-            <div className="state p-info">
-              <i className="icon"></i>
-              <label htmlFor={standard}>
-                {standard}
-              </label>
-            </div>
-          </div>
+          <button
+            className="standard-link"
+            onClick={() => this.something()}>
+            <p className="standard-text">{standard}</p>
+          </button>
         );
       } else {
         return (
