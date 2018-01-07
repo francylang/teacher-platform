e2123395fd3bfac5088fcf97819fd7663ae1d43d
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-01-07 11:17:20 -0700

    Render correct cards for standard filter and all

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
 import { PROD_URL } from '../url.js';
 
-class FilterSection extends Component {
-  filterDiscussions(standard) {
-    const { discussions, renderDiscussions, renderFilteredDiscussions } = this.props;
+const FilterSection = ({ renderFilteredDiscussions, renderDiscussions, domain }) => {
+  const fetchDiscussions = standard => {
+    fetch('http://localhost:3000/api/v1/discussions')
+      .then((response) => response.json())
+      .then((rawDiscussions) => findMatchingDiscussions(rawDiscussions, standard))
+      .then((matchingDiscussions) => {
+        renderDiscussions();
+        renderFilteredDiscussions(matchingDiscussions);
+      })
+      .catch((error) => console.error({ error }));
+  };
 
-    const filtered = discussions.filter(discussion => {
+  const findMatchingDiscussions = (rawDiscussions, standard) => {
+    return rawDiscussions.filter((discussion) => {
       if (standard === discussion.tagTitle) {
         return discussion;
       };
     });
+  };
 
-    renderDiscussions();
-    renderFilteredDiscussions(filtered);
-  }
+  const getFilteredDiscussions = standard => {
+    fetchDiscussions(standard);
+  };
 
-  buildDomainList() {
-    return this.props.domain.map(standard => {
+  const buildDomainList = () => {
+    return domain.map(standard => {
       let firstChar = standard.charAt(0);
 
       if (firstChar === '6' || firstChar === '7' || firstChar === '8') {
         return (
           <button
             className="standard-link"
-            onClick={() => this.filterDiscussions(standard)}
+            onClick={() => getFilteredDiscussions(standard)}
           >
             <p className="standard-text">
               {standard}
@@ -40,77 +50,13 @@ class FilterSection extends Component {
         );
       }
     });
-  }
+  };
 
-  render() {
-    return (
-      <aside className="domain-section">
-        {this.buildDomainList()}
-      </aside>
-    );
-  }
-}
+  return (
+    <aside className="domain-section">
+      {buildDomainList()}
+    </aside>
+  );
+};
 
 export default FilterSection;
-
-// import React from 'react';
-// import { PROD_URL } from '../url.js';
-//
-// const FilterSection = ({ renderFilteredDiscussions, renderDiscussions, domain }) => {
-//   const fetchDiscussions = (standard) => {
-//     console.log({standard});
-//     fetch(`${PROD_URL}/api/v1/discussions`)
-//       .then((response) => response.json())
-//       .then((rawDiscussions) => findMatchingDiscussions(rawDiscussions, standard))
-//       .then((matchingDiscussions) => {
-//         renderFilteredDiscussions(matchingDiscussions);
-//         renderDiscussions();
-//       })
-//       .catch((error) => console.error({ error }));
-//   };
-//
-//   const findMatchingDiscussions = (rawDiscussions, standard) => {
-//     return rawDiscussions.filter((discussion) => {
-//       return discussion.tagTitle === standard;
-//     });
-//   };
-//
-//   const getFilteredDiscussions = (standard) => {
-//     fetchDiscussions(standard);
-//   };
-//
-//   const buildDomainList = () => {
-//     return domain.map(standard => {
-//       let firstChar = standard.charAt(0);
-//
-//       if (firstChar === '6' || firstChar === '7' || firstChar === '8') {
-//         return (
-//           <button
-//             className="standard-link"
-//             onClick={() => getFilteredDiscussions(standard)}
-//           >
-//             <p className="standard-text">
-//               {standard}
-//             </p>
-//           </button>
-//         );
-//       } else {
-//         return (
-//           <section className="domain-label-container">
-//             <h5 className="domain-label">
-//               {standard}
-//             </h5>
-//           </section>
-//         );
-//       }
-//     });
-//   };
-//
-//   return (
-//     <aside className="domain-section">
-//       {buildDomainList()}
-//     </aside>
-//   );
-// };
-//
-// export default FilterSection;
