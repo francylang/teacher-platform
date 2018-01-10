5e69a414de75bbd135d3fd4f5faa20cc8bfc33ae
Francy Lang <francy.lang@gmail.com>
2018-01-10 15:39:42 -0700

    Standard tag will render as the color of it's domain

--- src/containers/DiscussionFormContainer.js
+++ src/containers/DiscussionFormContainer.js
@@ -2,15 +2,10 @@ import { connect } from 'react-redux';
 import DiscussionForm from '../components/DiscussionForm.jsx';
 
 const mapStateToProps = store => ({
-  // discussions: store.discussions,
-  // filteredDiscussions: store.filteredDiscussions,
-  // comments: store.comments,
+  discussions: store.discussions,
 });
 
 const mapDispatchToProps = dispatch => ({
-  // filterDiscussions: (filteredDiscussions, searchTerm) => {
-  //   dispatch(filterDiscussions(filteredDiscussions, searchTerm));
-  // }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DiscussionForm);
